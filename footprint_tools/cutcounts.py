ee603bbb8f2162b56f6e4fb18bf2ce6fe936206e
Jeff Vierstra <jeffrv@u.washington.edu>
2019-03-12 20:17:47 -0700

    Updated "cutounts" module to support fast access to allelic cutcounts

--- footprint_tools/cutcounts.py
+++ footprint_tools/cutcounts.py
@@ -3,21 +3,25 @@
 import pysam
 import numpy as np
 
+from collections import defaultdict
+
+class ReadError(Exception):
+	pass
+
+class GenotypeError(Exception):
+	pass
+
 class bamfile(object):
 
 	"""Class to access a BAM file (largely inspired/copied from Piper et al.)"""
 	
-	def __init__(self, filepath, min_qual = 1, remove_dups = False, remove_qcfail = True, chunksize = 1000, offset = (0, -1), use_cache = False):
+	def __init__(self, filepath, min_qual = 1, remove_dups = False, remove_qcfail = True, chunksize = 1000, offset = (0, -1)):
 
 		try:
 			self.samfile = pysam.Samfile(filepath, "rb")
 		except:
 
 			raise IOError("Cannot open BAM file: %s" % filepath)
-
-		self.use_cache = use_cache
-		self.cache = { i: {"+": {}, "-": {}} for i in self.samfile.references }
-		self.lookup = { i: [] for i in self.samfile.references }
 
 		self.offset = offset #-1 # a hack for the mis-aligned data from 2010
 		self.CHUNK_SIZE = chunksize
@@ -26,88 +30,123 @@ class bamfile(object):
 		self.remove_dups = remove_dups
 		self.remove_qcfail = remove_qcfail
 
-	def __add_to_cache(self, chrom, start, end):
-        
-		for alignedread in self.samfile.fetch(chrom, max(start - 10, 0), end + 10):
+	def __validate_read(self, read):
+		if self.remove_qcfail and read.is_qcfail:
+			raise ReadError()
+		if self.remove_dups and read.is_duplicate:
+			raise ReadError()
+		if read.mapping_quality < self.min_qual:
+			raise ReadError()
+
+		return read
+
+	def __get_read_mate(self, read):
+		fpos = self.samfile.tell()
+		try:
+			mate = self.samfile.mate(read)
+		except ValueError:
+			mate = None
+		finally:
+			self.samfile.seek(fpos)
+		return mate
+
+	def __read_pair_generator(self, chrom, start, end):
+		
+		read_dict = defaultdict(lambda: [None, None])
+
+		for read in self.samfile.fetch(chrom, max(start-10, 0), end+10):
 
 			try:
 
-				if alignedread.mapping_quality < self.min_qual:
-					continue
+				self.__validate_read(read)
 
-				if self.remove_dups and alignedread.is_duplicate:
-					continue
-
-				if self.remove_qcfail and alignedread.is_qcfail:
-					continue
-
-				if alignedread.is_reverse:
-					a = int(alignedread.reference_end) + self.offset[1]
-					if a < end:	
-						self.cache[chrom]["-"][a] = self.cache[chrom]["-"].get(a, 0.0) + 1.0
+				# if single-end just pass the read
+				if not read.is_paired:
+					yield read, None 
+				
+				# we think it is paired
 				else:
-					a = int(alignedread.reference_start) + self.offset[0] # pysam is zero-based!
-					if a >= start:
-						self.cache[chrom]["+"][a] = self.cache[chrom]["+"].get(a, 0.0) + 1.0
 
-			except:
+					if not read.is_proper_pair or read.is_secondary or read.is_supplementary:
+						continue
 
-				pass
-		
-		self.lookup[chrom].append(start)
+					qname = read.query_name
+					if qname not in read_dict:
+						if read.is_read1:
+							read_dict[qname][0] = read
+						else:
+							read_dict[qname][1] = read
+					else:
+						if read.is_read1:
+							yield read, read_dict[qname][1]
+						else:
+							yield read_dict[qname][0], read
+						del read_dict[qname]
 
-	def __lookup_from_cache(self, chrom, start, end):
+			except ReadError, e:
+				continue
 
-		lbound = int(np.floor(start / float(self.CHUNK_SIZE)) * float(self.CHUNK_SIZE))
-		ubound = int(np.ceil(end / float(self.CHUNK_SIZE)) * float(self.CHUNK_SIZE))
 
-		for i in range(lbound, ubound, self.CHUNK_SIZE):
-           		
-			if i not in self.lookup[chrom]:
-                		
-				self.__add_to_cache(chrom, i, i + self.CHUNK_SIZE)
-        
-		#fills in with zeroes where the hash table contains no information for each strand.
-		fw_cutarray  = np.array([self.cache[chrom]["+"].get(i, 0.0) for i in range(start, end)])
-		rev_cutarray = np.array([self.cache[chrom]["-"].get(i, 0.0) for i in range(start, end)])
+	def __add_read(self, read, fw, rev):
+		if read.is_reverse:
+			a = int(read.reference_end)+self.offset[1]
+			rev[a] = rev.get(a, 0.0) + 1.0
+		else:
+			a = int(read.reference_start)+self.offset[0]
+			fw[a] = fw.get(a, 0.0) + 1.0
 
-		return {"+": fw_cutarray, "-": rev_cutarray}
-	
-	def __lookup(self, chrom, start, end):
+	# DEPRECATED
+	# def __lookup(self, chrom, start, end, flip=False):
+
+	# 	tmp_fw = {}
+	# 	tmp_rev = {}
+
+	# 	for read in self.samfile.fetch(chrom, max(start-10, 0), end+10):
+			
+	# 		try:
+	# 			self.__validate_read(read)
+	# 			self.__add_read(read, tmp_fw, tmp_rev)
+
+	# 			#if read.is_reverse:
+	# 			#	a = int(read.reference_end) + self.offset[1]
+	# 			#	if a < end:	
+	# 			#		tmp_rev[a] = tmp_rev.get(a, 0.0) + 1.0
+	# 			#else:
+	# 			#	a = int(read.reference_start)  + self.offset[0] # pysam is zero-based!
+	# 			#	if a >= start:
+	# 			#		tmp_fw[a] = tmp_fw.get(a, 0.0) + 1.0
+
+	# 		except:
+	# 			pass
+
+	# 	fw_cutarray = np.array([tmp_fw.get(i, 0.0) for i in range(start, end)])
+	# 	rev_cutarray = np.array([tmp_rev.get(i, 0.0) for i in range(start, end)])
+
+	# 	return {
+	# 		"+": rev_cutarray[::-1] if flip else fw_cutarray, 
+	# 		"-": fw_cutarray[::-1] if flip else rev_cutarray
+	# 		}
+
+	def __lookup(self, chrom, start, end, flip=False):
 
 		tmp_fw = {}
 		tmp_rev = {}
 
-		for alignedread in self.samfile.fetch(chrom, max(start - 10, 0), end + 10):
-			
-			try:
+		for read1, read2 in self.__read_pair_generator(chrom, max(start-10, 0), end+10):
 
-				if alignedread.mapping_quality < self.min_qual:
-					continue
+			self.__add_read(read1, tmp_fw, tmp_rev)
+			if read2:
+				self.__add_read(read2, tmp_fw, tmp_rev)
 
-				if self.remove_dups and alignedread.is_duplicate:
-					continue
-
-				if self.remove_qcfail and alignedread.is_qcfail:
-					continue
-
-				if alignedread.is_reverse:
-					a = int(alignedread.reference_end) + self.offset[1]
-					if a < end:	
-						tmp_rev[a] = tmp_rev.get(a, 0.0) + 1.0
-				else:
-					a = int(alignedread.reference_start)  + self.offset[0] # pysam is zero-based!
-					if a >= start:
-						tmp_fw[a] = tmp_fw.get(a, 0.0) + 1.0
-
-			except:
-
-				pass
 
 		fw_cutarray = np.array([tmp_fw.get(i, 0.0) for i in range(start, end)])
 		rev_cutarray = np.array([tmp_rev.get(i, 0.0) for i in range(start, end)])
 
-		return {"+": fw_cutarray, "-": rev_cutarray}
+
+		return {
+ 			"+": rev_cutarray[::-1] if flip else fw_cutarray, 
+			"-": fw_cutarray[::-1] if flip else rev_cutarray
+			}
 
 	def __getitem__(self, interval):
 
@@ -116,12 +155,106 @@ class bamfile(object):
 		end = interval.end
 
 		flip = True if interval.strand == '-' else False
-		
 
-		ret = self.__lookup(chrom, start, end) if not self.use_cache else self.__lookup_from_cache(chrom, start, end)
-
-		if flip:
-			ret["+"], ret["-"] = ret["-"][::-1], ret["+"][::-1]
-
+#		ret = self.__lookup(chrom, start, end) if not self.use_cache else self.__lookup_from_cache(chrom, start, end)
+		ret = self.__lookup(chrom, start, end, flip)
 		return ret
 
+	def __lookup_allelic(self, chrom, start, end, pos, ref, alt, flip=False):
+
+		#visited_read_pairs = set()
+
+		tmp_ref_fw = {}
+		tmp_ref_rev = {}
+		tmp_alt_fw = {}
+		tmp_alt_rev = {}
+
+		for read1, read2 in self.__read_pair_generator(chrom, max(start-10, 0), end+10):
+
+			#try read1 for genotype
+			try:
+				
+				offset = pos-read1.reference_start
+				s = read1.query_sequence[offset]
+
+				mismatches = int(read1.get_tag("XM", with_value_type=False))
+
+				if s==ref and mismatches<=1:
+					fw=tmp_ref_fw
+					rev=tmp_ref_rev
+				elif s==alt and mismatches<=2:
+					fw=tmp_alt_fw
+					rev=tmp_alt_rev
+				else:
+					raise GenotypeError()
+
+				self.__add_read(read1, fw, rev)
+				if read2:
+					self.__add_read(read2, fw, rev)
+				continue
+
+			except GenotypeError, e:
+				continue
+			except IndexError, e:
+				pass
+
+
+			# if we make if here and a paired read is available lets try it
+			if not read2:
+				continue
+
+			#try read2 for genotype
+			try:
+				
+				offset = pos-read2.reference_start
+				s = read2.query_sequence[offset]
+
+				mismatches = int(read2.get_tag("XM", with_value_type=False))
+
+				if s==ref and mismatches<=1:
+					fw=tmp_ref_fw
+					rev=tmp_ref_rev
+				elif s==alt and mismatches<=2:
+					fw=tmp_alt_fw
+					rev=tmp_alt_rev
+				else:
+					raise GenotypeError()
+
+				self.__add_read(read1, fw, rev)
+				self.__add_read(read2, fw, rev)
+
+				continue
+
+			except GenotypeError, e:
+				continue
+			except IndexError, e:
+				pass
+
+		ref_fw_cutarray = np.array([tmp_ref_fw.get(i, 0.0) for i in range(start, end)])
+		ref_rev_cutarray = np.array([tmp_ref_rev.get(i, 0.0) for i in range(start, end)])
+		alt_fw_cutarray = np.array([tmp_alt_fw.get(i, 0.0) for i in range(start, end)])
+		alt_rev_cutarray = np.array([tmp_alt_rev.get(i, 0.0) for i in range(start, end)])
+
+		return {
+			ref: {
+				"+": ref_rev_cutarray[::-1] if flip else ref_fw_cutarray, 
+				"-": ref_fw_cutarray[::-1] if flip else ref_rev_cutarray
+				},
+			alt: {
+				"+": alt_rev_cutarray[::-1] if flip else alt_fw_cutarray, 
+				"-": alt_fw_cutarray[::-1] if flip else alt_rev_cutarray
+			}
+		}
+
+
+
+	def get_allelic_reads(self, interval, pos, ref, alt, flip=False):
+
+		chrom = interval.chrom
+		start = interval.start
+		end = interval.end
+
+		flip = True if interval.strand == '-' else False
+
+		ret = self.__lookup_allelic(chrom, start, end, pos, ref, alt, flip)
+		return ret
