a50b242a2ad65719254f6b5da9558f06e7914f84
Jeff Vierstra <jvierstra@altius.org>
2020-06-25 12:16:52 -0700

    PyPI python version

--- setup.py
+++ setup.py
@@ -68,6 +68,5 @@ setup(
 	    'License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)',
 	    'Programming Language :: Python :: 2.7',
 	    'Programming Language :: Python :: 3',
-	    'Programming Language :: Python :: 3.5', 
 ],
 )
