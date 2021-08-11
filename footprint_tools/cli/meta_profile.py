1ab976c0664859fdefc7775d4d175a9e0e4ccc20
Jeff Vierstra <jvierstra@altius.org>
2021-08-11 03:33:16 -0700

    Bugfix

--- footprint_tools/cli/meta_profile.py
+++ footprint_tools/cli/meta_profile.py
@@ -68,6 +68,6 @@ def run(interval_file, bam_file, n_threads=8):
     ds = profile_loader(interval_file, bam_file)
     #print(ds[0])
     
-    for x in tqdm(ds.batch_iter(batch_size=100, collate_fn=list_collate, num_workers=n_threads), colour='#00ff00')s:
+    for x in tqdm(ds.batch_iter(batch_size=100, collate_fn=list_collate, num_workers=n_threads), colour='#00ff00'):
         #print([len(a) for a in x['counts']])
         pass
