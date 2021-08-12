ad995658a899a8a2760824d1bf4a9fba95d97d53
Jeff Vierstra <jvierstra@altius.org>
2021-08-11 20:59:10 -0700

    Update

--- footprint_tools/cli/utils.py
+++ footprint_tools/cli/utils.py
@@ -1,17 +1,16 @@
+import click
 from argh.exceptions import CommandError
 
-def tuple_args(arg_type=int):
+def tuple_args(ctx, param, value):
     """
     Function to parser a tuple of integers from command line
     """
-    def parse_tuple(s):
-        try:
-            fw, rev = list(map(arg_type, s.split(',')))
-            return (fw, rev)
-        except Exception as e:
-            raise CommandError("Argument must be a comma-delimited tuple -- i.e., 0,-1")
-    return parse_tuple
+    items = value.split(',')
+    if len(items) > 2:
+        raise click.BadOptionUsage('needs to be a comma-delimiter tuple'))
 
+    return items
+    
 def list_args(arg_type=int):
     """
     Function to parser a list of integers from command line
