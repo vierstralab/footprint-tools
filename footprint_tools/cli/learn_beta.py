ba80e7e1205d09397b8b4d1164073a54e6665019
Jeff Vierstra <jvierstra@altius.org>
2021-08-11 19:13:19 -0700

    Update

--- footprint_tools/cli/learn_beta.py
+++ footprint_tools/cli/learn_beta.py
@@ -33,6 +33,8 @@ def run(bedgraph_file,
     total_lines = total_passed = 0
     obs_over_exp = []
 
+    logger.info(f"Filtering nucleotides with expected count <= {exp_cutoff} and within a FDR {fdr_cutoff} footprint")
+
     with yaspin(Spinners.bouncingBar, text='Reading nucleotides') as sp:
         
         filehandle = open(bedgraph_file, 'r')
@@ -40,7 +42,7 @@ def run(bedgraph_file,
         for line in filehandle:
             total_lines +=1
             if total_lines % 500000 == 0:
-                sp.text = "Reading nucleotides -- {:,} / {:,} in footprints".format(total_passed, total_lines)
+                sp.text = "Reading nucleotides -- {:,} / {:,} passed filters".format(total_passed, total_lines)
 
             fields = line.strip().split('\t')
             exp = float(fields[3])
@@ -60,6 +62,9 @@ def run(bedgraph_file,
         i = np.isfinite(obs_over_exp) & (obs_over_exp>0) & (obs_over_exp<1)
         
         prior = scipy.stats.beta.fit(obs_over_exp[i], floc = 0, fscale = 1)[0:2]
+
+    logger.info(f"{total_passed,:} positions used for fitting distribution")
+    logger.info(f"Beta distribution parameters - \u03b1 = {prior[0]}, \u03b2 = {prior[1]} ")
 
     print("%0.4f\t%0.4f" % (prior[0], prior[1]), file = sys.stdout)
 
