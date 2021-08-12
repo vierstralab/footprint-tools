ce9a5fb60fe1b9361f057ca1e2cc3153676342dc
Jeff Vierstra <jvierstra@altius.org>
2021-08-11 18:50:44 -0700

    Update 'learn_beta'

--- footprint_tools/cli/learn_beta.py
+++ footprint_tools/cli/learn_beta.py
@@ -2,8 +2,6 @@ import sys
 
 from argh.decorators import named, arg
 
-import pysam
-
 import numpy as np
 import scipy.stats
 
@@ -12,22 +10,6 @@ logger = logging.getLogger(__name__)
 
 from yaspin import yaspin
 from yaspin.spinners import Spinners
-
-def process_func(contig, tabix_filehandle, fdr_cutoff, exp_cutoff):
-    """
-    Fetch nucleotides per contig
-    """
-    res = []
-
-    for row in tabix_filehandle.fetch(contig=contig, parser=pysam.asTuple(), multiple_iterators=True):
-
-        exp = np.float64(row[3])
-        obs = np.float64(row[4])
-        fdr = np.float64(row[7])
-
-        if fdr <= fdr_cutoff and exp >= exp_cutoff:
-            res.append( (obs+1)/(exp+1) )
-    return res
 
 @named('learn_beta')
 @arg('bedgraph_file',
@@ -48,14 +30,37 @@ def run(bedgraph_file,
     Note: This step is required to compute posterior footprint probabilities.
     """
 
-    filehandle = open(bedgraph_file, 'r')
 
     total_lines = 0
-    with yaspin(Spinners.hamburger, text="0") as sp:
+    obs_over_exp = []
+
+    with yaspin(Spinners.bouncingBar, text="0") as sp:
+        
+        filehandle = open(bedgraph_file, 'r')
+
         for line in filehandle:
             total_lines +=1
-
             if total_lines % 1000000 == 0:
-                sp.text = "{:,}".format(total_lines)
+                sp.text = "Reading nucleotides {:,}".format(total_lines)
+
+            fields = line.strip().split('\t')
+            exp = int(fields[3])
+            obs = int(fields[4])
+            fdr = float(fields[7])
+
+            if fdr <= fdr_cutoff and exp >= exp_cutoff:
+                obs_over_exp.append( (obs+1)/(exp+1) )
+        
+        filehandle.close()
+        
+        sp.text = "Fitting Beta distribution"
+
+        obs_over_exp = np.array(obs_over_exp)
+
+        i = np.isfinite(obs_over_exp) & (obs_over_exp>0) & (obs_over_exp<1)
+        
+        prior = scipy.stats.beta.fit(obs_over_exp[i], floc = 0, fscale = 1)[0:2]
+
+        print("%0.4f\t%0.4f" % (prior[0], prior[1]), file = sys.stdout)
 
     return 0
