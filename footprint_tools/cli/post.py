dee0102c2e78e233695949fa55404850c4e36b78
Jeff Vierstra <jvierstra@altius.org>
2021-08-12 10:29:32 -0700

    Update

--- footprint_tools/cli/post.py
+++ footprint_tools/cli/post.py
@@ -14,7 +14,7 @@ from genome_tools import genomic_interval
 from footprint_tools.modeling import dispersion
 from footprint_tools.stats import posterior
 from footprint_tools.cli.utils import tuple_args, get_kwargs
-from footprint_tools.data.process import processor
+from footprint_tools.data.process import process
 from footprint_tools.data.utils import numpy_collate_concat
 
 from tqdm import tqdm
@@ -26,7 +26,7 @@ logger = logging.getLogger(__name__)
 # kill numpy warnings
 np.seterr(all="ignore")
 
-class posterior_stats(processor):
+class posterior_stats(process):
     def __init__(self, interval_file, samples_data, fdr_cutoff):
 
         self.intervals = pd.read_table(interval_file)
