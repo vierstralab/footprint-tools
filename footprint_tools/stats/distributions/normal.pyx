e3c06483d6d46f46cb80ca5110342f004f3f58ab
jvierstra <jvierstra@altius.org>
2026-06-26 18:38:00 -0700

    Add Normal distribution logpmf

---
+++ footprint_tools/stats/distributions/normal.pyx
@@ -0,0 +1,21 @@
+"""
+Implementation of normal distribution
+"""
+# cython: embedsignature=True
+
+import numpy as np
+
+cimport cython
+cimport numpy as np
+
+cdef extern from "math.h":
+    double pow(double) nogil
+    double log(double) nogil
+    double sqrt(double) nogil
+
+cdef extern from "hcephes.h":
+    double HCEPHES_PI
+
+cpdef data_type_t logpmf(data_type_t x, data_type_t mu, data_type_t sig2) nogil:
+	cdef data_type_t sig = sqrt(sig2)
+	return -0.5 * pow((x-mu)/sig), 2) - log(sig*np.sqrt(2*HCEPHES_PI))
