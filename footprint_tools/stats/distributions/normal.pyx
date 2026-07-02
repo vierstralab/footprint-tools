fe987a9565ecc562117ff82b5b2e84bad35e48f0
m1kss <boytsovs.av@gmail.com>
2026-07-01 20:19:16 -0700

    Fix syntax error in logpmf function for correct power calculation

--- footprint_tools/stats/distributions/normal.pyx
+++ footprint_tools/stats/distributions/normal.pyx
@@ -18,4 +18,4 @@ cdef extern from "hcephes.h":
 
 cpdef data_type_t logpmf(data_type_t x, data_type_t mu, data_type_t sig2) nogil:
 	cdef data_type_t sig = sqrt(sig2)
-	return -0.5 * pow((x-mu)/sig), 2) - log(sig*sqrt(2*HCEPHES_PI))
+	return -0.5 * pow((x-mu)/sig, 2) - log(sig*sqrt(2*HCEPHES_PI))
