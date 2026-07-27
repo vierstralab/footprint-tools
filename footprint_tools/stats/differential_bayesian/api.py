8829536da7e9053704bd5b4eaf24d9909dc00519
m1kss <boytsovs.av@gmail.com>
2026-07-27 10:52:48 -0700

    Fix bug in save_data_results

--- footprint_tools/stats/differential_bayesian/api.py
+++ footprint_tools/stats/differential_bayesian/api.py
@@ -87,9 +87,9 @@ def save_data_results(data, path, fields=None):
         "__interval__": values["__interval__"]
     }
 
-    for name, item in result_fields.items():
+    for name, in result_fields:
         payload[f"{name}.__class__"] = np.asarray(_DATA_RESULT_CLASSES[name].__name__)
-        for key, value in item.items():
+        for key, value in values[name].items():
             payload[f"{name}.{key}"] = value
 
     np.savez_compressed(path, **payload)
